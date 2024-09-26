b785a9a6e5ada41e37436d8656a6f5522906d7c6
knoopsr <knoopsr@gmail.com>
2024-09-26 20:18:46 +0200

    View Accounts Only

---
+++ HomeManager/View/Security/ucAccounts.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeManager.View.Security
+{
+    /// <summary>
+    /// Interaction logic for ucAccounts.xaml
+    /// </summary>
+    public partial class ucAccounts : UserControl
+    {
+        public ucAccounts()
+        {
+            InitializeComponent();
+        }
+    }
+}
