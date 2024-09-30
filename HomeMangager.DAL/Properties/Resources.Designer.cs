d04bc931db76d745ea441248b53a0fed96931160
knoopsr <knoopsr@gmail.com>
2024-09-30 19:02:25 +0200

    Rename Project MODEL DAL COMMON

--- HomeMangager.DAL/Properties/Resources.Designer.cs
+++ HomeMangager.DAL/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HomeMangager.DAL.Properties {
+namespace HomeManager.DAL.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace HomeMangager.DAL.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HomeMangager.DAL.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HomeManager.DAL.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- HomeMangager.DAL/clsDAL.cs
+++ HomeMangager.DAL/clsDAL.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeMangager.DAL
+namespace HomeManager.DAL
 {
     public class clsDAL
     {
