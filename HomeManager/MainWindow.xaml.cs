a8797ff031f6988dedd5f6dbb4748c7fb9a2d81d
knoopsr <knoopsr@gmail.com>
2024-10-02 21:34:26 +0200

    Dictionary aangemaakt. Styles, kleuren, path apart gezet MinHeight minwidth toegevoegd

--- HomeManager/MainWindow.xaml.cs
+++ HomeManager/MainWindow.xaml.cs
@@ -34,67 +34,6 @@ namespace HomeManager
             Storyboard hideMenu = (Storyboard)FindResource("HideMenu");
             hideMenu.Begin();
         }
-
-        //private void btnMenuPerson_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    stckPerson.Visibility = Visibility.Visible;
-        //}
-
-        //private void btnMenuPerson_Unchecked(object sender, RoutedEventArgs e)
-        //{
-
-        //    stckPerson.Visibility = Visibility.Collapsed;
-        //}
-
-        //private void btnBudget_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    stckBudget.Visibility = Visibility.Visible;
-        //}
-
-        //private void btnBudget_Unchecked(object sender, RoutedEventArgs e)
-        //{
-        //    stckBudget.Visibility = Visibility.Collapsed;
-        //}
-
-        //private void btnTodo_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    stckTodo.Visibility = Visibility.Visible;
-        //}
-
-        //private void btnTodo_Unchecked(object sender, RoutedEventArgs e)
-        //{
-        //    stckTodo.Visibility = Visibility.Collapsed;
-        //}
-
-        //private void btnSecurity_Checked(object sender, RoutedEventArgs e)
-        //{
-        //        stckSecurity.Visibility = Visibility.Visible;
-        //}
-
-        //private void btnSecurity_Unchecked(object sender, RoutedEventArgs e)
-        //{
-        //    stckSecurity.Visibility = Visibility.Collapsed;
-        //}
-
-        //private void btnStickyNotes_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    stckStickynotes.Visibility = Visibility.Visible;
-        //}
-
-        //private void btnStickyNotes_Unchecked(object sender, RoutedEventArgs e)
-        //{
-        //    stckStickynotes.Visibility = Visibility.Collapsed;
-        //}
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
- 
-            ucPersonen _ucPersonen = new ucPersonen();
-
-            Grid.SetRow(_ucPersonen, 1);
-            Grid.SetColumn(_ucPersonen, 1);
-
-            grdMain.Children.Add(_ucPersonen);
-        }
+        
     }
 }
