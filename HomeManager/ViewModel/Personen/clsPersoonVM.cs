b8c615378512be26a07d14c61208657b2b26514b
ThomasJacobs1996 <58427363+ThomasJacobs1996@users.noreply.github.com>
2024-10-29 21:48:06 +0100

    V2 aangepast met icons

---
+++ HomeManager/ViewModel/Personen/clsPersoonVM.cs
@@ -0,0 +1,13 @@
+﻿using HomeManager.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeManager.ViewModel
+{
+    public class clsPersoonVM : clsCommonModelPropertiesBase
+    {
+    }
+}
