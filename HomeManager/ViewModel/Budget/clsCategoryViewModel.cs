7414e870f2d3d7e70b69fe9aad785c5efd691d0e
TonyCira <acirafici@telenet.be>
2024-11-01 11:37:26 +0100

    Update

---
+++ HomeManager/ViewModel/Budget/clsCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeManager.Common;
+
+
+namespace HomeManager.ViewModel
+{
+    public class clsCategoryViewModel : clsCommonModelPropertiesBase
+    {
+
+    }
+}
