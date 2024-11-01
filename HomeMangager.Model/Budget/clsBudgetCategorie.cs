51bfdba4de294cb87bb2bf7fec8218e5ba6ff56d
TonyCira <acirafici@telenet.be>
2024-11-01 10:50:02 +0100

    Budget Categorie

---
+++ HomeMangager.Model/Budget/clsBudgetCategorie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeManager.Model.Budget
+{
+    public class clsBudgetCategorie
+    {
+        private int _BudgetCategorieID;
+        public int BudgetCategorieID 
+        { 
+            get 
+            {  
+                return _BudgetCategorieID; 
+            } 
+            set
+            {
+                _BudgetCategorieID = value;
+               
+            }
+        }
+
+        private string _BudgetCategorie;
+        public string BudgetCategorie
+        {
+            get
+            {
+                return _BudgetCategorie;
+            }
+            set
+            {
+                if (_BudgetCategorie != value)
+                {
+                    if (_BudgetCategorie != null)
+                    {
+                        //IsDirty = true;
+                    }
+                    _BudgetCategorie = value;
+                }
+            }
+
+        }
+    }
+}
