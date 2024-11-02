9f1eb5570d2de22ddbc7efd279c3b21cef31fad1
TonyCira <acirafici@telenet.be>
2024-11-02 23:29:34 +0100

    update

---
+++ HomeManager/DataService/Budget/clsCategorieDataService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeManager.Common;
+using HomeManager.DAL;
+using HomeManager.DAL.Budget;
+using HomeManager.Model.Budget;
+using Microsoft.Data.SqlClient;
+
+
+namespace HomeManager.DataService.Budget
+{
+    public class clsCategorieDataService : ICategorieDataService
+    {
+        ICategorieRepository Repo = new clsCategorieRepository();
+
+        public bool Delete(clsCategorieModel entity)
+        {
+            return Repo.Delete(entity);
+        }
+
+        public clsCategorieModel Find()
+        {
+            return Repo.Find();
+        }
+
+        public ObservableCollection<clsCategorieModel> GetAll()
+        {
+            return Repo.GetAll();
+        }
+
+        public clsCategorieModel GetById(int id)
+        {
+            return Repo.GetById(id);
+        }
+
+        public clsCategorieModel GetFirst()
+        {
+            return Repo.GetFirst();
+        }
+
+        public bool Insert (clsCategorieModel entity)
+        {
+            return Repo.Insert(entity);
+        }
+
+        public bool Update(clsCategorieModel entity)
+        {
+            return Repo.Update(entity);
+        }
+
+
+
+
+
+    }
+}
