7ec65357d2b1a7a14f23147c26e54b2bc85b8b1c
knoopsr <knoopsr@gmail.com>
2024-11-27 20:37:56 +0100

    clsPermissionChecker

--- HomeManager/Helpers/clsPermissionChecker.cs
+++ HomeManager/Helpers/clsPermissionChecker.cs
@@ -8,46 +8,38 @@ using System.Threading.Tasks;
 namespace HomeManager.Helpers
 {
     public class clsPermissionChecker
-    {
-        // Methode om te controleren of een specifieke code aanwezig is in RechtenCodes
+    {        
         public bool HasPermission(string code)
         {
-            // Haal de rechten codes op van de singleton instance van clsLoginModel
+           
             string rechtenCodes = clsLoginModel.Instance.RechtenCodes;
 
             if (string.IsNullOrEmpty(rechtenCodes))
             {
-                return false; // Als er geen rechten zijn ingesteld, dan meteen false retourneren
-            }
-
-            // Split de rechten codes op basis van de | teken
+                return false;
+            }  
             var rechtenLijst = rechtenCodes.Split('|');
-
-            // Controleer of de opgegeven code in de lijst voorkomt
             return rechtenLijst.Contains(code);
         }
 
         public bool PermissionViewmodel(string viewModel)
         {
-            // Controleer of er een rechtencode is voor de bestemming
+            
             if (_destinationToPermissionMap.TryGetValue(viewModel, out string requiredPermission))
             {
-                // Haal de rechten codes op van de singleton instance van clsLoginModel
+               
                 string rechtenCodes = clsLoginModel.Instance.RechtenCodes;
 
                 if (string.IsNullOrEmpty(rechtenCodes))
                 {
-                    return false; // Als er geen rechten zijn ingesteld, dan meteen false retourneren
+                    return false; 
                 }
-
-                // Split de rechten codes op basis van de | teken
+               
                 var rechtenLijst = rechtenCodes.Split('|');
 
-                // Controleer of de vereiste code in de lijst voorkomt
                 return rechtenLijst.Contains(requiredPermission);
             }
-
-            // Als er geen rechtencode is gekoppeld aan de bestemming, geef false terug
+            
             return false;
         }
 
@@ -56,7 +48,6 @@ namespace HomeManager.Helpers
 
 
 
-        // Dictionary met hardcoded koppelingen tussen bestemming en rechtencode
         private readonly Dictionary<string, string> _destinationToPermissionMap = new Dictionary<string, string>
         {
             {"clsPersoonVM", "100" },
