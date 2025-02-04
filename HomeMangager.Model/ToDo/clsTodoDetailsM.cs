6e477ced9fc68029cb0aabba725e2bf303d502e9
Ben Vanhoef <Ben.Vanhoef@brightest.be>
2025-02-04 20:21:43 +0100

    tododetails M

--- HomeMangager.Model/ToDo/clsTodoDetailsM.cs
+++ HomeMangager.Model/ToDo/clsTodoDetailsM.cs
@@ -1,12 +1,125 @@
-﻿using System;
+﻿using HomeManager.Common;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HomeManager.Model.Todo
 {
-    internal class clsTodoDetailsM
+    public class clsTodoDetailsM : clsCommonModelPropertiesBase, IDataErrorInfo
     {
+        private int _toDoDetailID;
+        public int ToDoDetailID
+        {
+            get
+            {
+                return _toDoDetailID;
+            }
+            set
+            {
+                _toDoDetailID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _toDoID;
+        public int ToDoID
+        {
+            get
+            {
+                return _toDoID;
+            }
+            set
+            {
+                _toDoID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _toDoDetail;
+        public string ToDoDetail
+        {
+            get
+            {
+                return _toDoDetail;
+            }
+            set
+            {
+                if (_toDoDetail != value)
+                {
+                    if (_toDoDetail != null)
+                    {
+                        IsDirty = true;
+                    }
+                    _toDoDetail = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private bool _isKlaar;
+        public bool IsKlaar
+        {
+            get
+            {
+                return _isKlaar;
+            }
+            set
+            {
+                _isKlaar = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int? _volgorde;
+        public int? Volgorde
+        {
+            get
+            {
+                return _volgorde;
+            }
+            set
+            {
+                _volgorde = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public override string ToString()
+        {
+            return ToDoDetail;
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = string.Empty;
+                switch (columnName)
+                {
+                    case nameof(ToDoDetail):
+                        if (string.IsNullOrWhiteSpace(_toDoDetail))
+                        {
+                            error = "Detail is een verplicht veld.";
+                            if (ErrorList.Contains(nameof(ToDoDetail)) == false)
+                            {
+                                ErrorList.Add(nameof(ToDoDetail));
+                            }
+                        }
+                        else if (_toDoDetail.Length > 50)
+                        {
+                            error = "Detail mag maximaal 50 karakters bevatten.";
+                            if (ErrorList.Contains(nameof(ToDoDetail)) == false)
+                            {
+                                ErrorList.Add(nameof(ToDoDetail));
+                            }
+                        }
+                        break;
+                }
+                return error;
+            }
+        }
     }
 }
