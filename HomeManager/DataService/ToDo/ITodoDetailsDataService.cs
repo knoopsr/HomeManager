39dd00394e858e57ee807bd4a6b5ebd2dde649dd
Ben Vanhoef <Ben.Vanhoef@brightest.be>
2025-02-04 20:53:28 +0100

    TodoDetails dataservice

---
+++ HomeManager/DataService/ToDo/ITodoDetailsDataService.cs
@@ -0,0 +1,14 @@
+﻿using HomeManager.Common;
+using HomeManager.Model.Todo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeManager.DataService.ToDo
+{
+    interface ITodoDetailsDataService : IDataService<clsTodoDetailsM>
+    {
+    }
+}
