08d11769a31849ac180fc731d832ba713847f26a
ThomasJacobs1996 <58427363+ThomasJacobs1996@users.noreply.github.com>
2025-04-02 18:59:51 +0200

    aanpassingen

--- HomeManager/ViewModel/Personen/clsEmailVerzendenViewModel.cs
+++ HomeManager/ViewModel/Personen/clsEmailVerzendenViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using HomeManager.DataService.Personen;
+using HomeManager.Helpers;
+using HomeManager.Mail;
+using HomeManager.MailService;
+using HomeManager.Messages;
 using HomeManager.Model.Personen;
 using HomeManager.Model.Security;
 using System;
@@ -7,6 +11,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using static HomeManager.ViewModel.clsPersonenViewModel;
 
 namespace HomeManager.ViewModel.Personen
 {
@@ -14,6 +21,7 @@ namespace HomeManager.ViewModel.Personen
     {
         clsEmailAdressenDataService VerzendenService;
 
+        public ICommand SubmitCommand { get; private set; }
 
         private ObservableCollection<clsEmailAdressenModel> _mijnVerzenderEmailAdres;
         public ObservableCollection<clsEmailAdressenModel> MijnVerzenderEmailAdres
@@ -22,13 +30,123 @@ namespace HomeManager.ViewModel.Personen
             set { _mijnVerzenderEmailAdres = value; }
         }
 
+        private clsEmailAdressenModel _mijnSelectedItem;
+        public clsEmailAdressenModel MijnSelectedItem
+        {
+            get { return _mijnSelectedItem; }
+            set
+            {
+                _mijnSelectedItem = value;
+                // Hier kun je eventueel extra logica toevoegen als de geselecteerde item verandert
+            }
+        }
+
 
 
         public clsEmailVerzendenViewModel()
         {
-            VerzendenService = new clsEmailAdressenDataService();
+            //VerzendenService = new clsEmailAdressenDataService();
 
+            //MijnVerzenderEmailAdres = VerzendenService.GetByPersoonID(clsLoginModel.Instance.PersoonID);
+
+            //// Initialiseer het commando
+            //SubmitCommand = new RelayCommand(ExecuteSubmit, CanExecuteSubmit);
+            ////Messenger
+            //clsMessenger.Default.Register<clsEmailVerzendenModel>(this, OnUpdateListMessageReceived);
+            VerzendenService = new clsEmailAdressenDataService();
             MijnVerzenderEmailAdres = VerzendenService.GetByPersoonID(clsLoginModel.Instance.PersoonID);
+
+            // Initialiseer MijnSelectedItem met het eerste item in de lijst, als deze niet leeg is
+            if (MijnVerzenderEmailAdres.Count > 0)
+            {
+                MijnSelectedItem = MijnVerzenderEmailAdres[0];
+            }
+
+            // Initialiseer het commando
+            SubmitCommand = new RelayCommand(ExecuteSubmit, CanExecuteSubmit);
+            //Messenger
+            clsMessenger.Default.Register<clsEmailVerzendenModel>(this, OnUpdateListMessageReceived);
+
+        }
+
+        private void OnUpdateListMessageReceived(clsEmailVerzendenModel obj)
+        {
+            Ontvanger = obj.Ontvanger;
+        }
+
+        private bool CanExecuteSubmit()
+        {
+            return true; 
+        }
+
+        private async void ExecuteSubmit()
+        {
+            //// Hier haal je de gegevens op en verzend je de e-mail
+            //clsMailModel mailModel = new clsMailModel
+            //{
+            //    MailFromEmail = MijnSelectedItem.PersoonID.ToString(),
+            //    MailToEmail = Ontvanger,
+            //    Subject = Onderwerp,
+            //    Body = Bericht
+            //};
+
+            //bool emailVerzonden = await clsMail.SendEmail(mailModel);
+
+            //if (!emailVerzonden)
+            //{
+            //    MessageBox.Show("Er is een fout opgetreden bij het versturen van de e-mail.");
+            //}
+            if (MijnSelectedItem == null)
+            {
+                MessageBox.Show("Selecteer een verzender.");
+                return;
+            }
+
+            clsMailModel mailModel = new clsMailModel
+            {
+                MailFromEmail = MijnSelectedItem.PersoonID.ToString(), // Zorg ervoor dat je hier het juiste e-mailadres gebruikt
+                MailToEmail = Ontvanger,
+                Subject = Onderwerp,
+                Body = Bericht
+            };
+
+            bool emailVerzonden = await clsMail.SendEmail(mailModel);
+
+            if (!emailVerzonden)
+            {
+                MessageBox.Show("Er is een fout opgetreden bij het versturen van de e-mail.");
+            }
+
+        }
+
+        private string _onderwerp;
+        public string Onderwerp
+        {
+            get { return _onderwerp; }
+            set
+            {
+                _onderwerp = value;
+            }
+        }
+
+        private string _bericht;
+        public string Bericht
+        {
+            get { return _bericht; }
+            set
+            {
+                _bericht = value;
+            }
+        }
+
+        private string _ontvanger;
+        public string Ontvanger
+        {
+            get { return _ontvanger; }
+            set
+            {
+                _ontvanger = value;
+            }
         }
     }
 }
---
+++ HomeMangager.Model/Personen/clsEmailVerzendenModel.cs
@@ -0,0 +1,105 @@
+﻿using HomeManager.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeManager.Model.Personen
+{
+    public class clsEmailVerzendenModel : clsCommonModelPropertiesBase
+    {
+        private int _persoonID;
+        public int PersoonID
+        {
+            get
+            {
+                return _persoonID;
+            }
+            set
+            {
+                _persoonID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _emailID;
+        public int EmailID
+        {
+            get
+            {
+                return _emailID;
+            }
+            set
+            {
+                _emailID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _ontvanger;
+
+        public string Ontvanger
+        {
+            get
+            {
+                return _ontvanger;
+            }
+            set
+            {
+                if (_ontvanger != value)
+                {
+                    if (_ontvanger != null)
+                    {
+                        IsDirty = true;
+                    }
+                }
+                _ontvanger = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _onderwerp;
+
+        public string Onderwerp
+        {
+            get
+            {
+                return _onderwerp;
+            }
+            set
+            {
+                if (_onderwerp != value)
+                {
+                    if (_onderwerp != null)
+                    {
+                        IsDirty = true;
+                    }
+                }
+                _onderwerp = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _bericht;
+        public string Bericht
+        {
+            get
+            {
+                return _bericht;
+            }
+            set
+            {
+                if (_bericht != value)
+                {
+                    if (_bericht != null)
+                    {
+                        IsDirty = true;
+                    }
+                }
+                _bericht = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
